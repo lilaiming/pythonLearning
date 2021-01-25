5bd6e5f7b583e57580c5898a7323c037d48acd0e
lilaiming <441314423@qq.com>
2021-01-25 11:21:36 +0800

    8

--- pytest.py
+++ pytest.py
@@ -2,8 +2,8 @@
 # 本脚由lilaiming编写，用于学习使用！
 # QQ:441314423
 
-print('welcome to qytang!111')
+print('welcome to qytang!')
 print('welcome to lenovo!')
 print('welcome to python!')
 
-print('github commit 7')
+print('github commit 8')
